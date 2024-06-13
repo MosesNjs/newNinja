5d8fdcd6182d9e8c8f57dafaf1e9a5e6de52b76c
MosesNjs <mosessnjoku@gmail.com>
2024-06-13 10:13:18 +0100

    now bout to learn about modules

--- oneNinja.js
+++ oneNinja.js
@@ -1,3 +1,14 @@
-const opps = 'ngnExt'
+// const opps = 'ngnExt'
+// console.log(opps);
 
-console.log(opps);
+setTimeout(() => {
+    console.log('vscode is pretty intelligent');
+    clearInterval(int)
+}, 3000); // runs code IN 3 seconds, clears the interval int below end.
+// basically, in the 3rd second, he runs
+const int = setInterval(() => {
+    console.log('put a d in her ribcage');
+}, 1000); // runs code EVERY second.
+
+console.log(__dirname);
+console.log(__filename);
